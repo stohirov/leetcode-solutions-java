10d7ad9f3df54f98fc91c08c1bee09e7aa64c2b7
stohirov <suxrobtoxirov001@gmail.com>
2026-06-07 18:13:23 +0500

    TwoSum

--- src/main/java/Solution1.java
+++ src/main/java/Solution1.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+import java.util.HashMap;
 
-public class Solution1 {
+class Solution1 {
+  public int[] twoSum(int[] nums, int target) {
+    HashMap<Integer, Integer> integerHashMap = new HashMap<>();
+    for (int i = 0; i < nums.length; i++) {
+      int diff = target - nums[i];
+      if (integerHashMap.containsKey(diff)) {
+        return new int[]{integerHashMap.get(diff), i};
+      } else {
+        integerHashMap.put(nums[i], i);
+      }
+    }
+    return new int[]{};
+  }
 }
