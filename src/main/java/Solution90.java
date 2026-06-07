aacfe9debe5d2a46677508893d4ad1fd69eaf790
stohirov <suxrobtoxirov001@gmail.com>
2026-06-08 01:58:00 +0500

    Subsets II

---
+++ src/main/java/Solution90.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Solution90 {
+  public List<List<Integer>> subsetsWithDup(int[] nums) {
+    Arrays.sort(nums);
+    List<List<Integer>> result = new ArrayList<>();
+    backtrack(nums, 0, new ArrayList<>(), result);
+    return result;
+  }
+
+  private void backtrack(int[] nums, int start, List<Integer> current,
+                         List<List<Integer>> result) {
+    result.add(new ArrayList<>(current));
+
+    for (int i = start; i < nums.length; i++) {
+      if (i > start && nums[i] == nums[i - 1]) {
+        continue;
+      }
+      current.add(nums[i]);
+      backtrack(nums, i + 1, current, result);
+      current.removeLast();
+    }
+  }
+}
