3bf99ac422a3c055f9b4545f7bf8ae811a96377c
stohirov <suxrobtoxirov001@gmail.com>
2026-06-07 18:16:14 +0500

    Add Two Numbers

---
+++ src/main/java/ListNode.java
@@ -0,0 +1,17 @@
+public class ListNode {
+
+  int val;
+  ListNode next;
+
+  ListNode() {
+  }
+
+  public ListNode(int val) {
+    this.val = val;
+  }
+
+  ListNode(int val, ListNode next) {
+    this.val = val;
+    this.next = next;
+  }
+}
