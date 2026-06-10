55dc58c9ec31a85d250fe319ca7d2307e46bd538
stohirov <suxrobtoxirov001@gmail.com>
2026-06-10 14:57:42 +0500

    update

---
+++ src/main/java/Solution112.java
@@ -0,0 +1,12 @@
+public class Solution112 {
+  public boolean hasPathSum(TreeNode root, int targetSum) {
+    if (root == null) {
+      return false;
+    }
+    if (root.left == null && root.right == null) {
+      return targetSum == root.val;
+    }
+    int remaining = targetSum - root.val;
+    return hasPathSum(root.left, remaining) || hasPathSum(root.right, remaining);
+  }
+}
