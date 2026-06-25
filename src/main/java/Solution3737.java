a6fbfc60e3a7bf53c1d48a9a47f1cadc27bd929b
stohirov <suxrobtoxirov001@gmail.com>
2026-06-25 09:51:34 +0500

    daily coding solution

---
+++ src/main/java/Solution3737.java
@@ -0,0 +1,18 @@
+public class Solution3737 {
+  public int countMajoritySubarrays(int[] nums, int target) {
+    int n = nums.length;
+    int count = 0;
+    for (int i = 0; i < n; i++) {
+      int c = 0;
+      for (int j = i; j < n; j++) {
+        if (nums[j] == target) {
+          c++;
+        }
+        if (2 * c > (j - i + 1)) {
+          count++;
+        }
+      }
+    }
+    return count;
+  }
+}
