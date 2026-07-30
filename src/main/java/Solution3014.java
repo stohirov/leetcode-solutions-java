1f3d6c2529ab6c7fd9afcc53f65fd6235675a924
stohirov <suxrobtoxirov001@gmail.com>
2026-07-30 10:24:08 +0500

    daily challenge

---
+++ src/main/java/Solution3014.java
@@ -0,0 +1,10 @@
+public class Solution3014 {
+
+  public int minimumPushes(String word) {
+    int pushes = 0;
+    for (int i = 0; i < word.length(); i++) {
+      pushes += i / 8 + 1;
+    }
+    return pushes;
+  }
+}
